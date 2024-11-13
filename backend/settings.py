83472a5ac8a35b2aad8403df4b61b145bf17e18c
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2024-11-13 01:41:59 -0600

    rollback settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -14,14 +14,8 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
-import cloudinary
 
 load_dotenv()
-
-cloudinary.config(cloud_name=os.getenv('CLOUD_NAME'),
-                  api_key=os.getenv('CLOUD_KEY'),
-                  api_secret=os.getenv('CLOUD_SECRET'),
-                )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,7 +66,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders'
 ]
-
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -156,10 +149,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 STATIC_URL = 'static/'
-MEDIA_URL = 'https://res.cloudinary.com/tu_cloud_name/image/upload/'
+MEDIA_URL = '/Media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:  
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
